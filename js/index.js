3a7e7b5e2d57384dc74833ad537b0269a9100978
adiucas <adi@ucabd.com>
2022-09-05 16:22:56 +0600

    initial

--- js/index.js
+++ js/index.js
@@ -1,43 +1,13 @@
 $('.first-option').hide();
 
-var $grid = $('.grid').isotope({
-  // options
-  itemSelector: '.grid-item',
-  layoutMode: 'fitRows'
-});
-
-$('.filter button').on("click", function(){
-	var value = $(this).attr('data-name');
-	$grid.isotope({
-		filter:value
-	});
-})
-
-$(document).ready(function(){
-  $(window).scroll(function(){
-    var scroll = $(window).scrollTop();
-    if (scroll > 60) {
-      $(".navbar").addClass("bg-nav")
-      $(".navbar").css("box-shadow" , "0 3px 3px #e91b2321");
-      $(".navbar-light .navbar-nav .nav-link").addClass("sticky-nav-link");
-      $(".navbar-light .navbar-nav .active>.nav-link").removeClass("sticky-nav-link");
-    }
-    else{
-      $(".navbar").removeClass("bg-nav")
-      $(".navbar-light .navbar-nav .nav-link").removeClass("sticky-nav-link");
-      $(".navbar").css("box-shadow" , "unset");
-    }
-  })
-})
-
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    responsiveClass:true,
-    autoplay: true,
-    autoplayTimeout: 3000,
-    autoplayHoverPause:true,
-    dots: false,
-    margin: 15,
-    center: true,
-    autoWidth:true
-})
+// $('.owl-carousel').owlCarousel({
+//     loop:true,
+//     responsiveClass:true,
+//     autoplay: true,
+//     autoplayTimeout: 3000,
+//     autoplayHoverPause:true,
+//     dots: false,
+//     margin: 15,
+//     center: true,
+//     autoWidth:true
+// })
--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
+$.getScript("js/aos.js",function(){
+  AOS.init({
+    offset: 0, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 1000, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: true, // whether elements should animate out while scrolling past them
+    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+  });
+});
+
 function includeHTML() {
   var z, i, elmnt, file, xhttp;
   /* Loop through a collection of all HTML elements: */
@@ -26,8 +38,81 @@ function includeHTML() {
   }
 }
 
-$('.first-option').hide();
-
 $(function () {
   $('[data-toggle="tooltip"]').tooltip()
 })
+
+
+
+$(document).ready(function() {
+  $("#otpBtn").click(function() {
+    var otpInput = $('.otp-input');
+
+    if (!otpInput.hasClass('active')) {
+      otpInput.addClass('show').outerWidth();
+      otpInput.addClass('active');
+      $(".get-otp-section").hide();
+    }
+  });
+
+  $("#loginBtn").click(function() {
+    var loginCard = $('.login-card');
+
+    if (!loginCard.hasClass('active')) {
+      loginCard.addClass('show').outerWidth();
+      loginCard.addClass('active');
+      loginCard.show();
+    }
+  });
+
+  $("#loginBtnsm").click(function() {
+    var loginCard = $('.login-card');
+
+    if (!loginCard.hasClass('active')) {
+      loginCard.addClass('show').outerWidth();
+      loginCard.addClass('active');
+      loginCard.show();
+    }
+  });
+
+  $(document).mouseup(function(e) 
+  {
+      var loginCardd = $(".login-card");
+
+      // if the target of the click isn't the container nor a descendant of the container
+      if (loginCardd.hasClass('active')) {
+        if (!loginCardd.is(e.target) && loginCardd.has(e.target).length === 0) 
+        {
+            loginCardd.hide();
+            loginCardd.removeClass('show');
+            loginCardd.removeClass('active');
+        }
+      }
+  });
+  
+});
+
+var timeoutHandle;
+    function countdown(minutes, seconds) {
+        function tick() {
+            var counter = document.getElementById("otpTimer");
+            counter.innerHTML = "OTP will expire in " +
+                minutes.toString() + ":" + (seconds < 10 ? "0" : "") + String(seconds);
+            seconds--;
+            if (seconds >= 0) {
+              document.getElementById("otpTimer").style.display = "block";
+              document.getElementById("resetBtn").style.display = "none";
+                timeoutHandle = setTimeout(tick, 1000);
+            } else {
+              document.getElementById("otpTimer").style.display = "none";
+              document.getElementById("resetBtn").style.display = "block";
+                if (minutes >= 1) {
+                    // countdown(mins-1);   never reach “00″ issue solved:Contributed by Victor Streithorst
+                    setTimeout(function () {
+                        countdown(minutes - 1, 59);
+                    }, 1000);
+                }
+            }
+        }
+        tick();
+    }
